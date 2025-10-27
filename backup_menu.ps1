5a098a848cc00089ac6c6ccccba6b6f978750bed
AlfonsoSanzGiner <asanzginer@hotmail.com>
2025-10-27 20:59:13 +0100

    Zip checkbox and improve New-Form-Item

--- backup_menu.ps1
+++ backup_menu.ps1
@@ -1,13 +1,14 @@
 Add-Type -AssemblyName System.Windows.Forms
 
 # -------------------- Configuration --------------------
+Set-Location -Path $PSScriptRoot    # Working directory for relative paths
 $configPath = "config.json"
 $config = try { If (Test-Path $configPath -PathType Leaf) { Get-Content -Raw $configPath | ConvertFrom-Json } Else { "" } } catch { @{} }
 $defaultSources = If ($config.sources -is [string]) { $config.sources } Else { "example.txt,C:\folder name" }
 $defaultDestination = If ($config.destination -is [string]) { $config.destination } Else { "Backups" }
 $defaultInterval = If ($config.interval -is [int]) { $config.interval } Else { 600 }   # in seconds
+$defaultCompression = If ($config.compression -is [bool]) { [bool]$config.compression } Else { $false }
 $minimizeToTray = If ($config.minimizeToTray -is [bool]) { $config.minimizeToTray } Else { $true }
-$compression = If ($config.compression -is [bool]) { [bool]$config.compression } Else { $false }
 
 function Test-Destination([string]$Destination, [string]$ErrorMsg = "") {
     if ([string]::IsNullOrWhiteSpace($Destination)) {
@@ -58,68 +59,57 @@ function Backup([string]$Type, [string]$Sources, [string]$Destination, [bool]$Co
 # -------------------- Form --------------------
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Backup tool"
-$form.Size = New-Object System.Drawing.Size(440, 360)
+$form.Size = New-Object System.Drawing.Size(500, 360)
 $form.StartPosition = "CenterScreen"
 $form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
 $form.MaximizeBox = $false
 [int[]]$rowPositions = @(30, 60, 90, 130)
 
-# Toolstrip
-$toolStrip = New-Object System.Windows.Forms.ToolStrip
-$form.Controls.Add($toolStrip)
-
-$saveConfigTool = New-Object System.Windows.Forms.ToolStripMenuItem
-$saveConfigTool.Text = "Save Config"
-$toolStrip.Items.Add($saveConfigTool) | Out-Null
-
-$openConfig = New-Object System.Windows.Forms.ToolStripMenuItem
-$openConfig.Text = "Open Config"
-$toolStrip.Items.Add($openConfig) | Out-Null
-
-$openSourceFolder = New-Object System.Windows.Forms.ToolStripMenuItem
-$openSourceFolder.Text = "Open Source"
-$toolStrip.Items.Add($openSourceFolder) | Out-Null
-
-$openBackupFolderTool = New-Object System.Windows.Forms.ToolStripMenuItem
-$openBackupFolderTool.Text = "Open Backups"
-$toolStrip.Items.Add($openBackupFolderTool) | Out-Null
-
-$restoreTool = New-Object System.Windows.Forms.ToolStripMenuItem
-$restoreTool.Text = "Restore"
-$toolStrip.Items.Add($restoreTool) | Out-Null
-
-function New-Form-Item($Form, $ItemType, [string]$Text, [int]$X, [int]$Y, [int]$Width, [int]$Height) {
+function New-Form-Item($Form, $ItemType, [string]$Text, [int[]]$Position, [int[]]$Size) {
     $item = New-Object $ItemType
-    $item.Text = $Text
-    $item.SetBounds($X, $Y, $Width, $Height)
-    $Form.Controls.Add($item)
+    if ($Text) { $item.Text = $Text }
+    if ($Position) { $item.Location = New-Object System.Drawing.Point($Position[0], $Position[1]) }
+    if ($Size) { $item.Size = New-Object System.Drawing.Size($Size[0], $Size[1]) }
+    $Form.Add($item) | Out-Null
     return $item
 }
 
+# Toolstrip
+$toolStrip = New-Form-Item $form.Controls System.Windows.Forms.ToolStrip
+$saveConfigTool = New-Form-Item $toolStrip.Items System.Windows.Forms.ToolStripMenuItem "Save Config"
+$openConfig = New-Form-Item $toolStrip.Items System.Windows.Forms.ToolStripMenuItem "Open Config"
+$openSourceFolder = New-Form-Item $toolStrip.Items System.Windows.Forms.ToolStripMenuItem "Open Source"
+$openBackupFolderTool = New-Form-Item $toolStrip.Items System.Windows.Forms.ToolStripMenuItem "Open Backups"
+$restoreTool = New-Form-Item $toolStrip.Items System.Windows.Forms.ToolStripMenuItem "Restore"
+
 # Sources
-New-Form-Item $form System.Windows.Forms.Label "Source(s):" 10 $rowPositions[0] 70 20 | Out-Null
-$sourceBox = New-Form-Item $form System.Windows.Forms.TextBox $defaultSources 80 $rowPositions[0] 260 20
-$sourcesButton = New-Form-Item $form System.Windows.Forms.Button "Browse" 350 $rowPositions[0] 60 20
+New-Form-Item $form.Controls System.Windows.Forms.Label "Source(s):" @(10, $rowPositions[0]) @(70, 20) | Out-Null
+$sourceBox = New-Form-Item $form.Controls System.Windows.Forms.TextBox $defaultSources @(80, $rowPositions[0]) @(320, 20)
+$sourcesButton = New-Form-Item $form.Controls System.Windows.Forms.Button "Browse" @(410, $rowPositions[0]) @(60, 20)
 $sourcesToolTip = New-Object System.Windows.Forms.ToolTip
 $sourcesToolTip.SetToolTip($sourceBox, "Can be mixed files and/or folders, separated by commas")
 
 # Destination
-New-Form-Item $form System.Windows.Forms.Label "Destination:" 10 $rowPositions[1] 70 20 | Out-Null
-$destinationBox = New-Form-Item $form System.Windows.Forms.TextBox $defaultDestination 80 $rowPositions[1] 260 20
-$destinationButton = New-Form-Item $form System.Windows.Forms.Button "Browse" 350 $rowPositions[1] 60 20
+New-Form-Item $form.Controls System.Windows.Forms.Label "Destination:" @(10, $rowPositions[1]) (70, 20) | Out-Null
+$destinationBox = New-Form-Item $form.Controls System.Windows.Forms.TextBox $defaultDestination @(80, $rowPositions[1]) (320, 20)
+$destinationButton = New-Form-Item $form.Controls System.Windows.Forms.Button "Browse" @(410, $rowPositions[1]) (60, 20)
 
 # Buttons
-$backupButton = New-Form-Item $form System.Windows.Forms.Button "Backup" 10 $rowPositions[2] 100 30
-$startButton = New-Form-Item $form System.Windows.Forms.Button "Start" 120 $rowPositions[2] 100 30
-$stopButton = New-Form-Item $form System.Windows.Forms.Button "Stop" 230 $rowPositions[2] 100 30
+$backupButton = New-Form-Item $form.Controls System.Windows.Forms.Button "Backup" @(10, $rowPositions[2]) (100, 30)
+$startButton = New-Form-Item $form.Controls System.Windows.Forms.Button "Start" @(120, $rowPositions[2]) (100, 30)
+$stopButton = New-Form-Item $form.Controls System.Windows.Forms.Button "Stop" @(230, $rowPositions[2]) (100, 30)
+
+# Compression
+$compressionBox = New-Form-Item $form.Controls System.Windows.Forms.CheckBox "Zip?" @(350, $rowPositions[2]) (50, 30)
+$compressionBox.Checked = $defaultCompression
 
 # Interval
-New-Form-Item $form System.Windows.Forms.Label "Interval(s):" 350 ($rowPositions[2] - 3) 60 15 | Out-Null
-$intervalBox = New-Form-Item $form System.Windows.Forms.TextBox $defaultInterval 350 ($rowPositions[2] + 12) 60 20
+New-Form-Item $form.Controls System.Windows.Forms.Label "Interval(s):" @(410, ($rowPositions[2] - 3)) @(60, 15) | Out-Null
+$intervalBox = New-Form-Item $form.Controls System.Windows.Forms.TextBox $defaultInterval @(410, ($rowPositions[2] + 12)) @(60, 20)
 $intervalBox.TextAlign = [System.Windows.Forms.HorizontalAlignment]::Center
 
 # Logging
-$logBox = New-Form-Item $form System.Windows.Forms.TextBox "" 10 $rowPositions[3] 400 180
+$logBox = New-Form-Item $form.Controls System.Windows.Forms.TextBox "" @(10, $rowPositions[3]) @(460, 180)
 $logBox.Multiline = $true
 $logBox.ScrollBars = "Vertical"
 $logBox.ReadOnly = $true
@@ -137,7 +127,7 @@ function Log([string]$Level, [string]$Msg, [bool]$NewLine = $true) {
 $timer = New-Object System.Windows.Forms.Timer
 $timer.Interval = $defaultInterval * 1000
 $timer.Add_Tick({
-        Backup "AUTO" $sourceBox.Text $destinationBox.Text $compression
+        Backup "AUTO" $sourceBox.Text $destinationBox.Text $compressionBox.Checked
     })
 
 $saveConfigTool.Add_Click({
@@ -146,8 +136,8 @@ $saveConfigTool.Add_Click({
             sources        = $sourceBox.Text
             destination    = $destinationBox.Text
             interval       = [int]$intervalBox.Text
+            compression    = $compressionBox.Checked
             minimizeToTray = $minimizeToTray
-            compression    = $compression
         }
         if (-not (Test-Path $configPath)) {
             New-Item -Path $configPath -ItemType File -Force | Out-Null
@@ -219,7 +209,7 @@ $destinationButton.Add_Click({
 
 $backupButton.Add_Click({
         Log "INFO" "Manual backup"
-        Backup "MANUAL" $sourceBox.Text $destinationBox.Text $compression
+        Backup "MANUAL" $sourceBox.Text $destinationBox.Text $compressionBox.Checked
     })
 
 $startButton.Add_Click({
@@ -229,7 +219,7 @@ $startButton.Add_Click({
             return
         }
         Log "INFO" "Starting backup"
-        Backup "AUTO" $sourceBox.Text $destinationBox.Text $compression
+        Backup "AUTO" $sourceBox.Text $destinationBox.Text $compressionBox.Checked
         $timer.Interval = [int]$intervalSec * 1000
         $timer.Start()
     })
